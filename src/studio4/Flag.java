2c217ae5ea092b13678c71d15b764e0c9a389f0d
贾顺文 <@ShunwenJia.dhcp.wustl.edu>
2022-10-12 17:18:56 -0500

    Panda Flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,27 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.filledSquare(0.5, 0.5, 1);
+		
+		StdDraw.setPenColor(255, 255, 255);
+		StdDraw.filledRectangle(0.5, 0.5, 0.5, 0.33);
+		
+		StdDraw.setPenColor(0, 0, 0);
+		StdDraw.circle(0.5,  0.5, 0.25);
+		
+		StdDraw.filledCircle(0.68,  0.68, 0.07);
+		StdDraw.filledCircle(0.32,  0.68, 0.07);
+		
+		StdDraw.filledCircle(0.60,  0.55, 0.05);
+		StdDraw.filledCircle(0.40,  0.55, 0.05);
+		
+		double [] x = {0.5, 0.45, 0.55};
+		double [] y = {0.40, 0.45, 0.45};
+		StdDraw.filledPolygon(x, y);
+		
+		StdDraw.line(0.5, 0.40, 0.5, 0.37);
+		StdDraw.line(0.5, 0.37, 0.40, 0.35);
+		StdDraw.line(0.5, 0.37, 0.60, 0.35);
 	}
 }
